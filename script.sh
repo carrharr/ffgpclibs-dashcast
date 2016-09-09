c6f066eebfc6a6804fd57c62b082bcf3d93d8fd4
master <master@macpro.local>
2016-09-09 22:20:36 +0100

    Tested on 2 clean ub16.04, dashcast live does output au/vi/a_init/v_init/mpd :)

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ sudo apt-get -y remove ffmpeg x264 libav-tools libvpx-dev libx264-dev gpac
 
 # Get the dependencies:
 sudo apt-get update
-sudo apt-get -y install build-essential checkinstall git libfaac-dev libgpac-dev \
+sudo apt-get -y install build-essential checkinstall git vim libfaac-dev libgpac-dev \
   libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libtheora-dev \
     libvorbis-dev pkg-config texi2html yasm zlib1g-dev libswscale-dev libavresample-dev \
 	libxv-dev libavdevice-dev
@@ -15,9 +15,11 @@ sudo apt-get -y install libx264-dev
 cd
 git clone git://git.videolan.org/git/x264.git x264
 cd x264
-./configure --enable-shared --disable-cli --enable-strip --disable-avs --disable-swscale --disable-lavf --disable-ffms --disable-gpac --disable-opencl
-make -j
-sudo make install
+./configure --enable-static 
+make 
+sudo checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | \
+	awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes \
+	--fstrans=no --default
 
 # Install AAC audio decoder
 cd
@@ -25,29 +27,28 @@ wget http://downloads.sourceforge.net/opencore-amr/fdk-aac-0.1.0.tar.gz
 tar xzvf fdk-aac-0.1.0.tar.gz
 cd fdk-aac-0.1.0
 ./configure
-make -j
-sudo checkinstall --pkgname=fdk-aac --pkgversion="0.1.0" --backup=no \
-  --deldoc=yes --fstrans=no --default
+make 
+sudo make install
 
-# Install VP8 video encoder and decoder.
-cd
-git clone --depth 1 https://chromium.googlesource.com/webm/libvpx
-cd libvpx
-./configure
-make -j
-sudo checkinstall --pkgname=libvpx --pkgversion="1:$(date +%Y%m%d%H%M)-git" --backup=no \
-  --deldoc=yes --fstrans=no --default
+# Add lavf support to x264
+cd ~/x264
+make distclean
+./configure --enable-static
+make
+sudo make install
 
 # Installing FFmpeg
 cd
-git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
-./configure --enable-shared --enable-pic --enable-gpl \
-	--enable-libfaac --enable-libmp3lame --enable-libvpx --enable-libfdk-aac \
-  --enable-libx264 --enable-nonfree --enable-version3 --enable-avresample
-make -j
+./configure --enable-shared --enable-pic --enable-libfaac --enable-gpl \
+	--enable-librtmp --enable-libmp3lame --enable-libfdk-aac \
+	--enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora \
+	--enable-libvorbis --enable-libx264 --enable-nonfree \
+	--enable-version3 --enable-avresample
+make 
 sudo make install
-hash x264 ffmpeg ffplay ffprobe
+hash ffmpeg
 
 # Optional: install qt-faststart
 # This is a useful tool if you're showing your H.264 in MP4 videos on the web. It relocates some data in the video to allow playback to begin before the file is completely downloaded. Usage: qt-faststart input.mp4 output.mp4.
